c3f2bf24a351d0627c657d31dca72b63b1a49779
Paul Turner <stepdaddy.t@gmail.com>
2021-08-09 06:49:24 -0700

    added api to routes

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 
 //apiDocs
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   fs.readFile("docs/apiDocs.json", (err, data) => {
     if (err) {
       res.status(400).json({
@@ -47,9 +47,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
-app.use("/", postRoutes);
-app.use("/", authRoutes);
-app.use("/", userRoutes);
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 app.use(function (err, req, res, next) {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: "Please sign in" });
